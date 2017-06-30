0f1cea579f0d4b1c5be07ef0b86274420be0adcd
minecrawler <me@marco-alka.de>
2017-06-30 16:42:20 +0200

    add Result

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const Result = require('rustify-js').Result;
+
 // this is just here as a temporary measure, until I find time to implement this.
 // Otherwise, it might block the boot of SHPS
 module.exports = class Dependency {
-    static init() { return new Dependency(); }
+    static init() { return Result.fromSuccess(new Dependency()); }
 };
